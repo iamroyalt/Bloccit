efde252f31e3fc979db92f5481ddecb4bcc544f8
Tracy Royal <TracyRoyal@mac.com>
2016-03-05 14:28:29 -0500

    Assignment-26 complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -17,4 +17,12 @@ require 'rails_helper'
        expect(response).to render_template("about")
      end
    end
- end
+
+   describe "GET faq" do
+      it "renders the faq template" do
+        get :faq
+        expect(response).to render_template("faq")
+      end
+    end
+
+end
