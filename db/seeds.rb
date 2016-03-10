cc0c26fba3c243e885846229b2813d20711c1550
Tracy Royal <TracyRoyal@mac.com>
2016-03-10 16:30:39 -0500

    Assignment-30 revised

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ require 'random_data'
  100.times do
     Comment.create!(
     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
+     body: RandomData.random_paragraph)
  end
-Post.find_or_create_by(title: "Unique post", body: "Unique comment")
-Comment.find_or_create_by body: "unique comment body"
+
+unique_post = Post.find_or_create_by(title: "Unique post", body: "Unique comment")
+Comment.find_or_create_by(post: unique_post, body: "unique comment body")
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
