b4f7cdbd1c3911e621ec20591bcbd03c686c1b89
Tracy Royal <TracyRoyal@mac.com>
2016-03-18 10:15:04 -0400

    Assingment-38

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,10 +9,14 @@ module SessionsHelper
      session[:user_id] = nil
    end
 
- #define current user which returns the current user of the application 
+ #define current user which returns the current user of the application
    def current_user
      User.find_by(id: session[:user_id])
    end
 
+   def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
 
 end
