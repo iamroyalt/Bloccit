4d6a93325172af36763e44f04400080c5f92e3f3
Tracy Royal <TracyRoyal@mac.com>
2016-03-23 11:52:19 -0400

    Checkpoint-41 revised

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  #let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
-   #let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+   let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
    #let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
    let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
-   let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
+
 
       it { is_expected.to belong_to(:post) }
       it { is_expected.to belong_to(:user) }
