418284ab34182cc655bcf0b1075b9587d1359249
Tracy Royal <TracyRoyal@mac.com>
2016-04-04 09:53:39 -0400

    Assignment-42 4/3 am

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,59 +8,60 @@ class CommentsController < ApplicationController
 #########Assignment-42##########
 #find the right post using the :post_id and then create a new comment which is assigned to the user
           #@post = Post.find(params[:post_id])
-          comment = @post.comments.new(comment_params)
-          comment.user = current_user
+    comment = @post.comments.new(comment_params)
+    comment.user = current_user
 
-          if comment.save
-              flash[:notice] = "Comment saved successfully."
-              redirect_to [@post.topic, @post]
+    if comment.save
+      flash[:notice] = "Comment saved successfully."
+      redirect_to [@post.topic, @post]
 
-          else
-            flash[:alert] = "Comment failed to save."
-            redirect_to [@post.topic, @post]
-       end
+    else
+      flash[:alert] = "Comment failed to save."
+      redirect_to [@post.topic, @post]
+    end
+  end
 #find the right topic using :topic_id and then create a new comment which is assigned to the user
-         #@topic = Topic.find(params[:topic_id])
-          comment = @topic.comments.new(comment_params)
-          comment.user = current_user
+#@topic = Topic.find(params[:topic_id])
+    comment = @topic.comments.new(comment_params)
+    comment.user = current_user
 
-        if comment.save
-         flash[:notice] = "Comment saved successfully."
-         redirect_to [@topic]
+    if comment.save
+      flash[:notice] = "Comment saved successfully."
+      redirect_to [@topic]
 
-        else
-         flash[:alert] = "Comment failed to save."
-         redirect_to [@topic]
-     end
+    else
+      flash[:alert] = "Comment failed to save."
+      redirect_to [@topic]
+    end
   end
 
 
   def destroy
 #following the logic above...identifying the right :post_id to delete
           #params[:post_id]
-          @post = Post.find(params[:post_id])
-          comment = @post.comments.find(params[:id])
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
 
-          if comment.destroy
-            flash[:notice] = "Comment was deleted."
-            redirect_to [@post.topic, @post]
-          else
-            flash[:alert] = "Comment couldn't be deleted. Try again."
-            redirect_to [@post.topic, @post]
-          end
-        end
+    if comment.destroy
+      flash[:notice] = "Comment was deleted."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:alert] = "Comment couldn't be deleted. Try again."
+      redirect_to [@post.topic, @post]
+    end
 
-         @topic = Topic.find(params[:topic_id])
-         comment = @topic.comments.find(params[:id])
 
-         if comment.destroy
-           flash[:notice] = "Comment was deleted."
-           redirect_to [@topic]
-         else
-           flash[:alert] = "Comment couldn't be deleted. Try again."
-           redirect_to [@topic]
-         end
-       end
+    @topic = Topic.find(params[:topic_id])
+      comment = @topic.comments.find(params[:id])
+
+    if comment.destroy
+      flash[:notice] = "Comment was deleted."
+      redirect_to [@topic]
+    else
+      flash[:alert] = "Comment couldn't be deleted. Try again."
+      redirect_to [@topic]
+   end
+end
 
 
 private
@@ -76,9 +77,9 @@ private
 
 #allows the comment owner or an admin user to delete the comment..redirects unauthorized users to post show view
      def authorize_user
-     comment = Comment.find(params[:id])
-       unless current_user == comment.user || current_user.admin?
-         flash[:alert] = "You do not have permission to delete a comment."
-         redirect_to [comment.post.topic, comment.post]
+       comment = Comment.find(params[:id])
+         unless current_user == comment.user || current_user.admin?
+           flash[:alert] = "You do not have permission to delete a comment."
+           redirect_to [comment.post.topic, comment.post]
        end
-     end
+end
